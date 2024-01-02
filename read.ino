2e24c1d80ace4dd328c2204c664fa071c2c2f0fd
Imam Pratama Setiady <88639642+mamprat@users.noreply.github.com>
2024-01-02 22:15:06 +0700

    sudah coba dirapihkan dan coba menambah usulan  (#1)
    * merapihkan Update write.ino
    
    * merapihkan Update read.ino
    
    * merapihkan Update FULL_20240102_0845.ino
    
    merapihkan code + menambahkan rumus di velocity

--- read.ino
+++ read.ino
@@ -1,13 +1,12 @@
 void readnex() {
   str_down = myNex.readStr("t6.txt");
   str_up = myNex.readStr("t7.txt");
-  // Serial.println(standarddown);
-  if(((str_down == "ERROR") || (str_down == "")) && ((str_up == "ERROR") || (str_up == ""))){
+  if (((str_down == "ERROR") || (str_down == "")) && ((str_up == "ERROR") || (str_up == ""))) {
     standarddown = standarddown;
     standardup = standardup;
-  }else{
+  } else {
     standarddown = str_down.toFloat();
     standardup = str_up.toFloat();
-    myNex.writeStr("t8.txt",save);
+    myNex.writeStr("t8.txt", save);
   }
 }
