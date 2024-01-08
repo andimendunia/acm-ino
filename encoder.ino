efd3f8dfa1db808b3fb6384d88d4971e21eb8d50
Imam Pratama Setiady <imampratamasetiady11@gmail.com>
2024-01-08 13:44:46 +0700

    pulsa sesuai dan speed dan rate oke
    bisa stop ketika encoder diam pulsa selalu counter ga balik ke 0

--- encoder.ino
+++ encoder.ino
@@ -1,44 +1,56 @@
 #include <Encoder.h>
 
 // Deklarasi Pin Encoder
-#define encoderA 2 //output A rotary encoder
-#define encoderB 3 //output B rotary encoder
+#define encoderA 2  //output A rotary encoder
+#define encoderB 3  //output B rotary encoder
 
-const float circ_mm   = 220.0; //Dari spek autonics, keliling roda dalam milimeter
-const int   ppr       = 2048;  //Dari spek autonics, pulse tiap putaran
-const float pitch_mm  = 330.0; //Jarak antar garis kuning sepatu yang ada di conveyor
+const float circ_mm   = 250.0;   //Dari spek autonics, keliling roda dalam milimeter
+const int ppr         = 100;           //Dari spek autonics, pulse tiap putaran
+const float pitch_mm  = 330.0;  //Jarak antar garis kuning sepatu yang ada di conveyor
+
+float rps             = 0;
+float speed           = 0;
+float rate_act        = 0;
+unsigned long time_last = 0;
 
 // Deklarasi objek encoder
 Encoder myEncoder(encoderA, encoderB);
 
 void setup() {
   Serial.begin(9600);
-  delay(1000); // Tambahkan delay untuk memastikan Serial Monitor siap
+  time_last = 0;
+  delay(1000);  // Tambahkan delay untuk memastikan Serial Monitor siap
 }
 
 void loop() {
-  // Baca nilai pulsa dari encoder
-  //long pulses = myEncoder.read();
+  if (millis() - time_last >= 1000) {
+    // Dummy baca pulse
+    // long pulses = 132;
 
-  // Dummy baca pulse
-  long pulses = 3072;
+    // Baca nilai pulsa dari encoder
+    long pulses = myEncoder.read();
 
-  // Hitung jarak berdasarkan jumlah pulsa
-  float distance = (pulses * circ_mm) / ppr;
+    rps = pulses / ppr;
+    speed = round(rps * circ_mm);
+    if (speed <= 0) {
+      rate_act = 0;
+    } else {
+      rate_act = round(pitch_mm / speed);
+    }
+    // Hitung jarak berdasarkan jumlah pulsa
+    float distance = (pulses * circ_mm) / ppr;
 
-//  // Hitung kecepatan conveyor
-//  float speed = pulses * circ_mm / (ppr * pitch_mm);
-//
-//  // Hitung jumlah increment
-//  int rate = pulses / (ppr / pitch_mm);
-
-  // Tampilkan informasi pada Serial Monitor
-  Serial.print("Pulse: ");
-  Serial.print(pulses);
-  Serial.print(" | distance: ");
-  Serial.print(distance);
-  Serial.println(" mm");
-
-  // Tunggu sejenak sebelum membaca ulang
-  delay(1000);
+    // Tampilkan informasi pada Serial Monitor
+    Serial.print("Pulse: ");
+    Serial.print(pulses);
+    // Serial.print(" | distance: ");
+    // Serial.print(distance);
+    Serial.print(" | Speed: ");
+    Serial.print(speed);
+    Serial.print(" mm/s| rate_act: ");
+    Serial.print(rate_act);
+    Serial.println(" sec/prs ");
+    // Tunggu sejenak sebelum membaca ulang
+    time_last = millis();
+  }
 }
