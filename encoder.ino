a9783861fb64b0c64f60a67e77c95e3958f931f7
Imam Pratama Setiady <88639642+mamprat@users.noreply.github.com>
2024-01-08 19:34:59 +0700

    membatasi pulse
    ketika mencapai 3072 maka akan kembali menjadi 0 lagi

--- encoder.ino
+++ encoder.ino
@@ -1,56 +1,59 @@
 #include <Encoder.h>
 
 // Deklarasi Pin Encoder
-#define encoderA 2  //output A rotary encoder
-#define encoderB 3  //output B rotary encoder
+#define encoderA 2 //output A rotary encoder
+#define encoderB 3 //output B rotary encoder
 
-const float circ_mm   = 250.0;   //Dari spek autonics, keliling roda dalam milimeter
-const int ppr         = 100;           //Dari spek autonics, pulse tiap putaran
-const float pitch_mm  = 330.0;  //Jarak antar garis kuning sepatu yang ada di conveyor
-
-float rps             = 0;
-float speed           = 0;
-float rate_act        = 0;
-unsigned long time_last = 0;
+const float circ_mm   = 220.0; //Dari spek autonics, keliling roda dalam milimeter
+const int   ppr       = 2048;  //Dari spek autonics, pulse tiap putaran
+const float pitch_mm  = 330.0; //Jarak antar garis kuning sepatu yang ada di conveyor
 
 // Deklarasi objek encoder
 Encoder myEncoder(encoderA, encoderB);
 
+// Deklarasi awal pulse dibaca dari 0
+volatile long pulses  = 0;
+
+// Callback function
+void callback() {
+  pulses  = 0;
+}
+
+// Encoder interrupts ketika mencapai nilai pulsa = 3072
+void interrupts_encoder() {
+  pulses++;
+  if (pulses == 3072) {
+    callback();
+  }
+}
+
 void setup() {
   Serial.begin(9600);
-  time_last = 0;
-  delay(1000);  // Tambahkan delay untuk memastikan Serial Monitor siap
+  attachInterrupt(digitalPinToInterrupt(2), interrupts_encoder, RISING);
+  delay(1000); // Tambahkan delay untuk memastikan Serial Monitor siap
 }
 
 void loop() {
-  if (millis() - time_last >= 1000) {
-    // Dummy baca pulse
-    // long pulses = 132;
+  // Baca nilai pulsa dari encoder
+  //  long pulses = myEncoder.read();
+  // Dummy baca pulse
+  long pulses = 3072;
 
-    // Baca nilai pulsa dari encoder
-    long pulses = myEncoder.read();
+  float distance = (pulses * circ_mm) / ppr;
 
-    rps = pulses / ppr;
-    speed = round(rps * circ_mm);
-    if (speed <= 0) {
-      rate_act = 0;
-    } else {
-      rate_act = round(pitch_mm / speed);
-    }
-    // Hitung jarak berdasarkan jumlah pulsa
-    float distance = (pulses * circ_mm) / ppr;
+  //  // Hitung kecepatan conveyor
+  //  float speed = pulses * circ_mm / (ppr * pitch_mm);
 
-    // Tampilkan informasi pada Serial Monitor
-    Serial.print("Pulse: ");
-    Serial.print(pulses);
-    // Serial.print(" | distance: ");
-    // Serial.print(distance);
-    Serial.print(" | Speed: ");
-    Serial.print(speed);
-    Serial.print(" mm/s| rate_act: ");
-    Serial.print(rate_act);
-    Serial.println(" sec/prs ");
-    // Tunggu sejenak sebelum membaca ulang
-    time_last = millis();
-  }
+  // Tampilkan informasi pada Serial Monitor
+  Serial.print("Pulse: ");
+  Serial.print(pulses);
+  Serial.print(" | Speed: ");
+  //  Serial.print(speed);
+  //  Serial.print("mm/s");
+  Serial.print(" | distance: ");
+  Serial.print(distance);
+  Serial.println(" mm");
+
+  // Tunggu sejenak sebelum membaca ulang
+  delay(1000);
 }
